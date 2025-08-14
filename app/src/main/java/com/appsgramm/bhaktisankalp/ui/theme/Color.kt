838f5dcfaf70d8aafe4bdfc5589f7cd57c21c171
Pragati <apps.gramm@gmail.com>
2025-08-14 23:53:48 +0530

    Add Color.kt file

---
+++ app/src/main/java/com/appsgramm/bhaktisankalp/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.appsgramm.bhaktisankalp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Orange80 = Color(0xFFFFA500)
+val OrangeGrey80 = Color(0xFFEE9A49)
+val OrangeDark80 = Color(0xFFCC7722)
+
+val Orange40 = Color(0xFFFF8C00)
+val OrangeGrey40 = Color(0xFFCD853F)
+val OrangeDark40 = Color(0xFFD2691E)
+
+val Orange = Color(0xFFFFA500)
+val OrangeDark = Color(0xFFFF8C00)
+val OrangeLight = Color(0xFFFFB347)
